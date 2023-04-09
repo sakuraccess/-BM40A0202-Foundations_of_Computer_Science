c010f26ea7ea973ffd9e7d4c4157533bfb61ee0a
nagisa <Carrotsoft0925@outlook.com>
2023-04-09 17:27:02 +0300

    Update Task 5.py

--- Exercise 11/Task 5.py
+++ Exercise 11/Task 5.py
@@ -6,7 +6,6 @@ input_sets = input("Please enter the set, separated by spaces: \n")
 
 sets = [int(x) for x in input_sets.split(" ")]
 
-
 n = int(input("Please enter the total sum: \n"))
 
 for i in range(1, len(sets)+1):
